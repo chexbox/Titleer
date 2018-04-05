401ce5c0e684831310aad5135a1caa29ed1ad780
chexbox <chexbox@mailinator.com>
2018-04-05 15:32:52 -0500

    Updated Manifest and background files. Added crx file.

--- background.js
+++ background.js
@@ -6,6 +6,3 @@ chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
 chrome.commands.onCommand.addListener(function(title) {
   chrome.tabs.executeScript(null, {code: "document.title = Math.round(Math.random() * 1000000)"});
 });
-chrome.commands.onCommand.addListener(function(edit) {
-  chrome.tabs.executeScript(null, {code: "document.designMode='on';"});
-});
